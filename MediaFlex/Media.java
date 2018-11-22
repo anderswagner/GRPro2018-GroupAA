415f22c1238496493f8ca72925460fc723a76aa3
CptFinn <anders.pw@hotmail.com>
2018-11-22 15:56:42 +0100

    Added classes and added a bit functionality to Media abstract class

--- MediaFlex/Media.java
+++ MediaFlex/Media.java
@@ -1,6 +1,32 @@
-public class Media{
-    public Media(){
-        public static void test(){
-            System.out.print("Viktor sutter");
+import java.util.List;
+import java.util.ArrayList;
+public abstract class Media{
+    protected String name;
+    protected List<String> categories;
+    protected int year;
+    protected float rating;
+
+    public String getName(){
+        return name;
+    }
+
+    public List<String> getCategories(){
+        return categories;
+    }
+
+    public int getYear(){
+        return year;
+    }
+
+    public float getRating(){
+        return rating;
+    }
+
+    public boolean isCategory(String categoryToCheck){
+        for(String category : categories){
+            if (categoryToCheck.equals(category))
+                return true;
+        }
+        return false;
     }
 }
