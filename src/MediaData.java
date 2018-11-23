17e67141faadd23cd599f33604d3eb5a4944650e
CptFinn <anders.pw@hotmail.com>
2018-11-23 09:45:30 +0100

    MediaData can now return the ImageFileName of media

--- src/MediaData.java
+++ src/MediaData.java
@@ -1,18 +1,34 @@
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MediaData{
-    public List<Movie> movies;
+    private List<Movie> movies;
+    private List<Series> series;
+    private Map<String,Media> allMedia;
 
     private MediaParser mediaParser;
 
     public MediaData(){
         mediaParser = new MediaParser("ExternalData/film.txt", "ExternalData/serier.txt");
+        allMedia = new HashMap<String,Media>();
         try{
             movies = mediaParser.GetMovies();
-            System.out.println(movies.get(0));
+            series = mediaParser.GetSeries();
         } catch (IOException e){
             System.out.println(e.getMessage() + " couldn't parse objects");
+        } finally {
+            for(Media specificMovie : movies){
+                allMedia.put(specificMovie.getName(),specificMovie);
+            }
+            for(Media specificSeries : series){
+                allMedia.put(specificSeries.getName(),specificSeries);
+            }
         }
+    }
+
+    public String GetImageString(String mediaName){
+        return allMedia.get(mediaName).getImageFilename();
     }
 }
